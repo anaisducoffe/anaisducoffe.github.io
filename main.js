0099e7b0353562786bb0eaabc9fd09b655fe86c4
Olivier Giammetta <olivier.giammetta@wildmoka.com>
2022-01-29 14:47:33 +0100

    fix touch events

--- main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const flipCard = document.getElementById("flip-card");
-  flipCard.addEventListener("touchend", () => {
-    flipCard.classList.toggle("flipped");
-  });
-});
