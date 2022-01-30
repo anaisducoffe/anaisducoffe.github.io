5b37ac0145bada66ad4194b9b1867a9c11f8e9fa
Olivier Giammetta <olivier.giammetta@wildmoka.com>
2022-01-30 12:26:44 +0100

    try new workflow to publish page

--- webpack.common.js
+++ webpack.common.js
@@ -31,6 +31,6 @@ module.exports = {
   output: {
     clean: true,
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
   },
 };
